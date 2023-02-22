c13160f28434d5a8af0318fc190cf92c40314c4e
titran3 <121971162+titran3@users.noreply.github.com>
2023-02-22 11:02:51 -0800

    Finished 4.1

---
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        focalPoint = GameObject.Find("Focal Point");
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+}
